9b294375c0d7d84212e83c4b77152adb9b033337
Phebe Polk <pgreenwo@gmail.com>
2015-09-29 20:07:59 -0700

    Update examplePage1.js

--- app/components/examplePage1/examplePage1.js
+++ app/components/examplePage1/examplePage1.js
@@ -11,3 +11,12 @@
     .controller('ExamplePage1Controller', ['$scope', '$http', ExamplePage1Controller]);
 
 })();
+function drawChart() {
+      var data = google.visualization.arrayToDataTable([
+        ['Year', 'Visitations', { role: 'style' } ],
+        ['2010', 10, 'color: gray'],
+        ['2010', 14, 'color: #76A7FA'],
+        ['2020', 16, 'opacity: 0.2'],
+        ['2040', 22, 'stroke-color: #703593; stroke-width: 4; fill-color: #C5A5CF'],
+        ['2040', 28, 'stroke-color: #871B47; stroke-opacity: 0.6; stroke-width: 8; fill-color: #BC5679; fill-opacity: 0.2']
+      ]);
